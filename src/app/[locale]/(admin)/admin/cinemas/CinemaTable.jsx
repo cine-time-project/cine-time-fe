e6c269a2622e9e53ecf72ba407d40a4b435b4056
Yasar TAY <yasartay@gmail.com>
2025-10-30 23:55:03 +0100

    making progress

--- src/app/[locale]/(admin)/admin/cinemas/CinemaTable.jsx
+++ src/app/[locale]/(admin)/admin/cinemas/CinemaTable.jsx
@@ -2,53 +2,114 @@ import React, { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Paginator } from "primereact/paginator";
-import { Button as BootstrapButton } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
-export const CinemaTable = ({ data, currentPage, totalPages, onPageChange }) => {
+export const CinemaTable = ({
+  data,
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
   const [cinemas, setCinemas] = useState([]);
-  const [first, setFirst] = useState(0); // DataTable için index
-  const rows = 8; // sayfa başına kayıt sayısı
+  const [first, setFirst] = useState(0);
+  const rows = 8;
+  const [selectedCinemas, setSelectedCinemas] = useState([]);
 
   useEffect(() => {
     setCinemas(data);
-    setFirst(currentPage * rows); // DataTable indexi güncelle
+    setFirst(currentPage * rows);
   }, [data, currentPage]);
 
-  const imageBodyTemplate = (cinema) => (
-    <img src={cinema.imageUrl} className="w-6rem shadow-2 border-round" />
+  const selectedCinemaCount = selectedCinemas.length;
+
+  const header = (
+    <div>
+      <div className="text-dark h1">Cinemas</div>
+      <div className="d-flex flex-wrap align-items-center justify-content-between">
+        <div className="d-flex gap-2">
+          <Button
+            className="btn btn-danger"
+            disabled={selectedCinemaCount === 0}
+          >
+            <i className="pi pi-trash"></i> Delete
+            {selectedCinemaCount > 0 && (
+              <span> - {selectedCinemaCount} Cinema(s)</span>
+            )}
+          </Button>
+        </div>
+
+        <Button className="btn btn-success">
+          <i className="pi pi-plus"></i> New
+        </Button>
+      </div>
+    </div>
   );
 
+  const indexBodyTemplate = (rowData, options) => {
+    return first + options.rowIndex + 1;
+  };
+
+  const imageBodyTemplate = (cinema) => (
+    <Card.Img
+      src={cinema.imageUrl}
+      className="w-6rem shadow-2 border-round object-fit-cover"
+      height={50}
+    />
+  );
+
+  const hallBodyTemplate = (cinema) => {
+    const halls = cinema?.hall
+  }
+
   const actionBodyTemplate = (cinema) => (
-    <div className="d-flex gap-2 justify-content-around">
-      <BootstrapButton variant="success" size="sm">
-        <i className="pi pi-file-edit"></i>
-      </BootstrapButton>
-      <BootstrapButton variant="danger" size="sm">
-        <i className="pi pi-trash"></i>
-      </BootstrapButton>
+    <div className="d-flex gap-5 justify-content-end">
+      <Button className="btn btn-warning">
+        <i className="pi pi-file-edit"></i> Edit
+      </Button>
     </div>
   );
 
   const onPageChangeHandler = (event) => {
-    const newPage = event.page; // 0-indexed
+    const newPage = event.page;
     onPageChange(newPage);
   };
 
   return (
     <div className="card">
-      <DataTable value={cinemas} stripedRows tableStyle={{ minWidth: "50rem" }}>
-        <Column field="id" header="ID"></Column>
-        <Column field="name" header="Name"></Column>
-        <Column body={imageBodyTemplate} header="Image"></Column>
-        <Column body={(row) => row.city?.name || "-"} header="City"></Column>
-        <Column body={(row) => row.country?.name || "-"} header="Country"></Column>
-        <Column header="Actions" body={actionBodyTemplate}></Column>
+      <DataTable
+        value={cinemas}
+        header={header}
+        dataKey="id"
+        selectionMode="checkbox"
+        selection={selectedCinemas}
+        onSelectionChange={(e) => setSelectedCinemas(e.value)}
+        stripedRows
+        removableSort
+        tableStyle={{ minWidth: "50rem" }}
+      >
+        <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
+        <Column
+          header="#"
+          body={indexBodyTemplate}
+          style={{ width: "4rem", textAlign: "center" }}
+          headerStyle={{ textAlign: "center" }}  
+        />
+        <Column body={imageBodyTemplate} header="Image" />
+        <Column field="id" sortable header="ID" />
+        <Column field="name" sortable header="Name" />
+        <Column body={(row) => row.city?.name || "-"} sortable header="City" />
+        <Column
+          body={(row) => row.country?.name || "-"}
+          sortable
+          header="Country"
+        />
+        <Column body={actionBodyTemplate}></Column>
       </DataTable>
 
       <Paginator
         first={first}
         rows={rows}
-        totalRecords={totalPages * rows} // backend’den toplam sayfa * sayfa başı
+        totalRecords={totalPages * rows}
         onPageChange={onPageChangeHandler}
         template="PrevPageLink PageLinks NextPageLink"
         className="mt-3"
